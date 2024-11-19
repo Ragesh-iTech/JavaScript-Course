abd8675f12718fa3c902aef66f5580961a1126c5
Ragesh-iTech <ragesh12082005@gmail.com>
2024-11-19 18:29:44 +0530

    javascript

---
+++ JAVASCRIPT/OUR-CODE/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,38 @@
+
+var greet = 'Hello', who = 'world'
+console.log("%s, %s!", greet + " " + who);
+
+console.log({
+    'Email': '',
+    'Groups': {},
+    'Id': 33,
+    'IsHiddenInUI': false,
+    'IsSiteAdmin': false,
+    'LoginName': 'i:0#.w|virtualdomain\\user2',
+})
+
+
+
+// const employees = [
+//     { id:1, name:"ragesh", age:19, exp:8, salary:100000 },
+//     { id:2, name:"raj", age:20, exp:5, salary:20000 },
+//     { id:3, name:"main", age:22, exp:2, salary:20000 },
+//     { id:4, name:"ram", age:28, exp:6, salary:10000 },
+//     { id:3, name:"kumar", age:20, exp:4, salary:40000 },
+// ]
+
+// const minExp = 5;
+// const exp1 = employees.filter((employee) => {
+//     return employee.exp >= minExp;
+// })
+// const single = exp1[0];
+// single.name = 'ramakrishanan'
+// console.log(single);
+// console.log(exp1);
+// // const minSalary = 25000;
+// // const output = employees.filter((employee) => {
+// //     return employee.salary >= minSalary 
+// // })
+// // console.log(output);
+
+
